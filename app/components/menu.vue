73ce537c3594711d92d7a635a0d4a687bbd344b4
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-04 14:04:47 +0100

    Create components folder and menu and project-list components

---
+++ app/components/menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="font-mono">
+    <ul class="flex space-x-6">
+      <li>
+        <NuxtLink to="/" class="link">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="postcss" scoped>
+.link {
+  @apply p-1 hover:bg-green-100
+}
+</style>
