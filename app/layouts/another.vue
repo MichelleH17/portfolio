dafa27eb63e291bf3354b5fe5535e56c5860de67
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-04 14:03:45 +0100

    Create layouts folder and default and another layouts

---
+++ app/layouts/another.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    This is another layout
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Michaela Havlíková</NuxtLink>
+      </div>
+
+      <Menu />
+    </header>
+
+    <main class="mt-10 px-2">
+      <slot />    
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  titleTemplate: '%s - Michaela Havlíková',
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
+
+<style>
+body {
+  font-family: 'Roboto';
+}
+</style>
