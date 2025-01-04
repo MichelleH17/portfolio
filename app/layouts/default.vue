4247c52bcfc1a1f4362f60047c3590c7ff8677bd
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-04 19:31:52 +0100

    Add ClientOnly and ColorModeSelector components, add body styles

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
-        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Michaela Havlíková</NuxtLink>
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold hover:bg-green-100 dark:hover:bg-green-300 px-2 py-1 hover:dark:text-gray-800">Michaela Havlíková</NuxtLink>
+        </div>
+
+        <Menu />
       </div>
 
-      <Menu />
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
 
     <main class="mt-10 px-2">
@@ -31,8 +37,12 @@ useHead({
 })
 </script>
 
-<style>
+<style lang="postcss">
 body {
   font-family: 'Roboto';
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
 }
 </style>
