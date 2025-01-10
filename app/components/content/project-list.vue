0441136f5f6b0b1fc41898fdd24ef221bffa4676
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-10 22:55:49 +0100

    Move file inside content folder, delete text, use not-prose for styling

---
+++ app/components/content/project-list.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="not-prose">
+    <section v-if="pending">Loading...</section>
+    <section v-else-if="error">Something went wrong... Try again</section>
+    <section v-else>
+      <ul class="grid grid-cols-1 gap-4">
+        <li v-for="repo in data" :key="repo.id" class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono">
+          <a :href="repo.html_url" target="_blank">
+            <div class="font-semibold">{{ repo.name }}</div>
+            <p class="text-sm">{{ repo.description }}</p>
+          </a>
+        </li>    
+      </ul>
+    </section>  
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { error, pending, data } = await useFetch('https://api.github.com/users/MichelleH17/repos')
+</script>
+
+<style>
+
+</style>
